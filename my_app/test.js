4dbe2c47288e643e3d32f51356386a835faa7ff8
Mohiuddin <mohiuddin35-2894@diu.edu.bd>
2022-07-02 22:35:43 +0600

    class-2/7/2022

---
+++ my_app/test.js
@@ -0,0 +1,34 @@
+
+
+
+class Human{
+    constructor(Age){
+        this.age = Age;
+       
+    }
+    print(){
+        console.log(this.Age);
+    }
+
+}
+class User extends Human{
+    constructor(firstName,lastName){
+        super(20);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    print(){
+        console.log(this.firstName,"",this.lastName);
+    }
+
+}
+let newUser = new User("John","Doe");
+console.log(newUser);
+
+const userr ={
+    fName: "Mohi",
+    lName: "Doe"
+    print(){
+        console.log(this.fName,"",this.lName);
+    }
+}
